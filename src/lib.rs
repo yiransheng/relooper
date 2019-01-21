29a8446ce217ee0277a0e30a323d99c4955c3eb1
Yiran Sheng <shengyiran@gmail.com>
2019-01-20 20:31:10 -0800

    add test cases

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,15 @@ pub use crate::types::{BlockId, ShapeId};
 
 pub mod generic_c_like;
 
+/// # Very BASIC control flows
+///
+/// These tests operate on generated string representation of pseduo code.
+/// They will need to be updated if additional optimization has been added
+/// to Relooper.
 #[cfg(test)]
-mod tests {
+mod test_basics {
+    use std::iter::Peekable;
+
     use super::Relooper;
     use crate::generic_c_like::{AstKind, CLikeAst};
 
@@ -19,9 +26,9 @@ mod tests {
     fn test_basics_simple_x3() {
         let mut relooper: Relooper<String> = Relooper::new();
 
-        let a = relooper.add_block("// block a".to_string());
-        let b = relooper.add_block("// block b".to_string());
-        let c = relooper.add_block("// block c".to_string());
+        let a = relooper.add_block("__block__a".to_string());
+        let b = relooper.add_block("__block__b".to_string());
+        let c = relooper.add_block("__block__c".to_string());
 
         // FIXME
         relooper.add_branch(a, b, Some("a -> b".to_string()));
@@ -30,9 +37,9 @@ mod tests {
         let ast: CLikeAst = relooper.render(a).expect("Did not get shape");
 
         let expected_ast = AstKind::Seq(vec![
-            AstKind::Node("// block a".to_string()),
-            AstKind::Node("// block b".to_string()),
-            AstKind::Node("// block c".to_string()),
+            AstKind::Node("__block__a".to_string()),
+            AstKind::Node("__block__b".to_string()),
+            AstKind::Node("__block__c".to_string()),
         ])
         .into();
 
@@ -44,9 +51,9 @@ mod tests {
         let mut relooper: Relooper<String> = Relooper::new();
 
         let a = relooper.add_block("i = 0".to_string());
-        let b = relooper.add_block("// block b".to_string());
+        let b = relooper.add_block("__block__b".to_string());
         let c = relooper.add_block("i = i + 1".to_string());
-        let d = relooper.add_block("// block d".to_string());
+        let d = relooper.add_block("__block__d".to_string());
 
         relooper.add_branch(a, b, None);
         relooper.add_branch(b, c, None);
@@ -55,15 +62,148 @@ mod tests {
 
         let ast: CLikeAst = relooper.render(a).expect("Did not get shape");
 
-        println!("{}", ast);
+        let pseudo_code = ast.to_string();
+        let expected = "
+	    i = 0
+	    L1: while (true) {
+	      __block__b
+	      if ( i > 10 ) {
+		break L1;
+	      }
+	      i = i + 1
+	      continue L1;
+	    }
+	    __block__d
+        ";
 
-        let expected_ast = AstKind::Seq(vec![
-            AstKind::Node("// block a".to_string()),
-            AstKind::Node("// block b".to_string()),
-            AstKind::Node("// block c".to_string()),
-        ])
-        .into();
-
-        assert_eq!(ast, expected_ast);
+        assert!(
+            code_equal(pseudo_code.trim().chars(), expected.trim().chars()),
+            pseudo_code
+        );
     }
+
+    #[test]
+    fn test_basics_if_else() {
+        let mut relooper: Relooper<&'static str> = Relooper::new();
+
+        let a = relooper.add_block("__block__a");
+        let b = relooper.add_block("__block__b");
+        let c = relooper.add_block("__block__c");
+
+        relooper.add_branch(a, b, Some("true"));
+        relooper.add_branch(a, c, None);
+
+        let ast: CLikeAst = relooper.render(a).expect("Did not get shape");
+
+        let pseudo_code = ast.to_string();
+        let expected = "
+	    __block__a
+	    if ( true ) {
+	      __block__b
+	    }
+	    else {
+	      __block__c
+	    }
+        ";
+
+        assert!(
+            code_equal(pseudo_code.trim().chars(), expected.trim().chars()),
+            pseudo_code
+        );
+    }
+
+    #[test]
+    fn test_basics_if() {
+        let mut relooper: Relooper<&'static str> = Relooper::new();
+
+        let a = relooper.add_block("__block__a");
+        let b = relooper.add_block("__block__b");
+        let c = relooper.add_block("__block__c");
+
+        relooper.add_branch(a, b, Some("true"));
+        relooper.add_branch(a, c, None);
+        relooper.add_branch(b, c, None);
+
+        let ast: CLikeAst = relooper.render(a).expect("Did not get shape");
+
+        let pseudo_code = ast.to_string();
+        let expected = "
+	    L1: do {
+	      __block__a
+	      if ( true ) {
+		__block__b
+		break L1;
+	      }
+	      else {
+		break L1;
+	      }
+	    } while(false)
+	    __block__c
+        ";
+
+        assert!(
+            code_equal(pseudo_code.trim().chars(), expected.trim().chars()),
+            pseudo_code
+        );
+    }
+
+    // replace consecutive white spaces with a single space
+    struct InsignificantWhites<I: Iterator> {
+        chars: Peekable<I>,
+    }
+    impl<I: Iterator<Item = char>> InsignificantWhites<I> {
+        fn new(chars: I) -> Self {
+            InsignificantWhites {
+                chars: chars.peekable(),
+            }
+        }
+    }
+    impl<I: Iterator<Item = char>> Iterator for InsignificantWhites<I> {
+        type Item = char;
+
+        fn next(&mut self) -> Option<char> {
+            let mut ret = None;
+            while let Some(c) = self.chars.peek() {
+                if !c.is_whitespace() {
+                    if ret.is_none() {
+                        ret = self.chars.next();
+                    }
+                    break;
+                }
+
+                ret = Some(' ');
+                self.chars.next();
+            }
+
+            ret
+        }
+    }
+
+    // Generic c like AST is used for unit test purposes
+    // generally, white spaces are insignificant in this
+    // style of pseudo code, by comparing generated pseudo
+    // code, we can ensure relooper produces rightly "shaped"
+    // code.
+    fn code_equal<A, B>(x: A, y: B) -> bool
+    where
+        A: IntoIterator<Item = char>,
+        B: IntoIterator<Item = char>,
+    {
+        let mut x = InsignificantWhites {
+            chars: x.into_iter().peekable(),
+        };
+        let mut y = InsignificantWhites {
+            chars: y.into_iter().peekable(),
+        };
+        loop {
+            let c1 = x.next();
+            let c2 = y.next();
+            match (c1, c2) {
+                (Some(c1), Some(c2)) if c1 == c2 => continue,
+                (None, None) => return true,
+                _ => return false,
+            }
+        }
+    }
+
 }
