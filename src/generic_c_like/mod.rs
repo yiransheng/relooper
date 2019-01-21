6f1a7820d2cfebb995ebc8fc0a77809eb6572e54
Yiran Sheng <shengyiran@gmail.com>
2019-01-20 17:51:24 -0800

    make generic c-like ast configurable somewhat

--- src/generic_c_like/mod.rs
+++ src/generic_c_like/mod.rs
@@ -1,96 +1,164 @@
 mod pretty_print;
 
+use std::fmt;
+use std::marker::PhantomData;
+
 use crate::{BlockId, CondType, Exit, Flow, Relooper, ShapeId, StructedAst};
 
 #[derive(Debug)]
-pub struct GenericCLike<F, G> {
-    ast: Ast,
-    loop_prefix: F,
-    block_prefix: G,
+pub struct CLikeAst<C = DefaultConfig> {
+    kind: AstKind,
+    _config: PhantomData<C>,
 }
 
-#[derive(Debug)]
-enum Ast {
+pub struct DefaultConfig;
+
+impl StaticAstConfig for DefaultConfig {
+    fn config() -> AstConfig<'static> {
+        AstConfig::default()
+    }
+}
+
+pub trait StaticAstConfig {
+    fn config() -> AstConfig<'static>;
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct AstConfig<'a> {
+    pub label_prefix: &'a str,
+    pub loop_prefix: &'a str,
+    pub loop_postfix: &'a str,
+    pub labed_block_prefix: &'a str,
+    pub labed_block_postfix: &'a str,
+    pub control_variable: &'a str,
+}
+
+impl<'a> Default for AstConfig<'a> {
+    fn default() -> Self {
+        AstConfig {
+            label_prefix: "L",
+            loop_prefix: "while (true) {",
+            loop_postfix: "}",
+            labed_block_prefix: "do {",
+            labed_block_postfix: "} while(false)",
+            control_variable: "__label__",
+        }
+    }
+}
+
+impl<C: StaticAstConfig> From<AstKind> for CLikeAst<C> {
+    fn from(kind: AstKind) -> Self {
+        CLikeAst {
+            kind,
+            _config: PhantomData,
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum AstKind {
     Panic,
     Node(String),
-    If(String, Box<Ast>),
-    ElseIf(String, Box<Ast>),
-    Else(Box<Ast>),
-    Loop(usize, Box<Ast>),
-    Block(Option<usize>, Vec<Ast>),
+    If(String, Box<AstKind>),
+    ElseIf(String, Box<AstKind>),
+    Else(Box<AstKind>),
+    Loop(usize, Box<AstKind>),
+    LabeledBlock(usize, Box<AstKind>),
+    Seq(Vec<AstKind>),
 }
 
-impl StructedAst for Ast {
+impl<C: StaticAstConfig> StructedAst for CLikeAst<C> {
     type Expr = str;
 
     fn trap() -> Self {
-        Ast::Panic
+        AstKind::Panic.into()
     }
 
     fn expr(expr: &Self::Expr) -> Self {
-        Ast::Node(expr.to_string())
+        AstKind::Node(expr.to_string()).into()
     }
 
     fn exit(exit: Exit) -> Self {
+        let c = C::config();
+
         let label = if let Some(bid) = exit.set_label {
-            format!("__label__ = {}\n", bid.index())
+            format!("{} = {};\n", c.control_variable, bid.index())
         } else {
-            "\n".to_string()
+            "".to_string()
         };
 
         match exit.flow {
-            Flow::Direct => Ast::Node(label),
+            Flow::Direct => AstKind::Node(label).into(),
             Flow::Continue(Some(shape)) => {
-                let code = format!("{}continue 'L{}", label, shape.index());
-                Ast::Node(code)
+                let code = format!(
+                    "{}continue {}{};",
+                    label,
+                    c.label_prefix,
+                    shape.index()
+                );
+                AstKind::Node(code).into()
             }
             Flow::Continue(None) => {
-                let code = format!("{}continue", label);
-                Ast::Node(code)
+                let code = format!("{}continue;", label);
+                AstKind::Node(code).into()
             }
             Flow::Break(Some(shape)) => {
-                let code = format!("{}break 'L{}", label, shape.index());
-                Ast::Node(code)
+                let code = format!(
+                    "{}break {}{};",
+                    label,
+                    c.label_prefix,
+                    shape.index()
+                );
+                AstKind::Node(code).into()
             }
             Flow::Break(None) => {
-                let code = format!("{}break", label);
-                Ast::Node(code)
+                let code = format!("{}break;", label);
+                AstKind::Node(code).into()
             }
         }
     }
 
     fn join(mut self, other: Self) -> Self {
-        match &mut self {
-            Ast::Block(_, xs) => {
-                xs.push(other);
+        match &mut self.kind {
+            AstKind::Seq(xs) => {
+                xs.push(other.kind);
                 self
             }
-            _ => Ast::Block(None, vec![self, other]),
+            _ => AstKind::Seq(vec![self.kind, other.kind]).into(),
         }
     }
 
     fn wrap_in_loop(self, shape_id: ShapeId) -> Self {
         let id = shape_id.index();
-        Ast::Loop(id, Box::new(self))
+        AstKind::Loop(id, Box::new(self.kind)).into()
     }
 
     fn wrap_in_block(self, shape_id: ShapeId) -> Self {
         let id = shape_id.index();
-        Ast::Block(Some(id), vec![self])
+        AstKind::LabeledBlock(id, Box::new(self.kind)).into()
     }
 
     fn handled(self, cond_type: CondType<&Self::Expr>) -> Self {
+        let c = C::config();
+
         match cond_type {
-            CondType::If(c) => Ast::If(c.to_string(), Box::new(self)),
-            CondType::ElseIf(c) => Ast::ElseIf(c.to_string(), Box::new(self)),
-            CondType::IfLabel(bid) => {
-                Ast::If(format!("__label__ == {}", bid.index()), Box::new(self))
+            CondType::If(c) => {
+                AstKind::If(c.to_string(), Box::new(self.kind)).into()
             }
-            CondType::ElseIfLabel(bid) => Ast::ElseIf(
-                format!("__label__ == {}", bid.index()),
-                Box::new(self),
-            ),
-            CondType::Else => Ast::Else(Box::new(self)),
+            CondType::ElseIf(c) => {
+                AstKind::ElseIf(c.to_string(), Box::new(self.kind)).into()
+            }
+            CondType::IfLabel(bid) => AstKind::If(
+                format!("{} == {}", c.control_variable, bid.index()),
+                Box::new(self.kind),
+            )
+            .into(),
+            CondType::ElseIfLabel(bid) => AstKind::ElseIf(
+                format!("{} == {}", c.control_variable, bid.index()),
+                Box::new(self.kind),
+            )
+            .into(),
+            CondType::Else => AstKind::Else(Box::new(self.kind)).into(),
         }
     }
 }
--- src/generic_c_like/pretty_print.rs
+++ src/generic_c_like/pretty_print.rs
@@ -1,26 +1,108 @@
+use std::fmt;
 use std::io;
 
-pub struct PrettyFormatter<'a> {
+use super::{AstConfig, AstKind, CLikeAst, StaticAstConfig};
+
+impl<C: StaticAstConfig> CLikeAst<C> {
+    pub fn to_writer_pretty<W: io::Write>(
+        &self,
+        writer: &mut W,
+    ) -> Result<(), io::Error> {
+        let config = C::config();
+        let mut pretty_formatter = PrettyFormatter::with_indent("  ", config);
+        self.kind.pretty_fmt(&mut pretty_formatter, writer)
+    }
+}
+
+impl<C: StaticAstConfig> fmt::Display for CLikeAst<C> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        self.to_writer_pretty(&mut FmtWriter { inner: f })
+            .map_err(|_| fmt::Error::default())
+    }
+}
+
+// this is weird...
+struct FmtWriter<'a, W> {
+    inner: &'a mut W,
+}
+impl<'a, W: fmt::Write> FmtWriter<'a, W> {
+    fn io_error(s: &'static str) -> io::Error {
+        io::Error::new(io::ErrorKind::Other, s)
+    }
+}
+impl<'a, W: fmt::Write> io::Write for FmtWriter<'a, W> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let n = buf.len();
+        let s = ::std::str::from_utf8(buf)
+            .map_err(|_| Self::io_error("Utf8 Error"))?;
+        self.inner
+            .write_str(s)
+            .map_err(|_| Self::io_error("fmt Error"))?;
+
+        Ok(n)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+impl AstKind {
+    fn pretty_fmt<W: io::Write>(
+        &self,
+        f: &mut PrettyFormatter,
+        writer: &mut W,
+    ) -> Result<(), io::Error> {
+        match self {
+            AstKind::Node(s) => f.write_node(s, writer),
+            AstKind::Panic => f.write_node("unreachable!()", writer),
+            AstKind::If(s, inner) => {
+                f.write_if(s, writer)?;
+                inner.pretty_fmt(f, writer)?;
+                f.write_block_postfix(writer)
+            }
+            AstKind::ElseIf(s, inner) => {
+                f.write_else_if(s, writer)?;
+                inner.pretty_fmt(f, writer)?;
+                f.write_block_postfix(writer)
+            }
+            AstKind::Else(inner) => {
+                f.write_else(writer)?;
+                inner.pretty_fmt(f, writer)?;
+                f.write_block_postfix(writer)
+            }
+            AstKind::Loop(id, inner) => {
+                f.write_loop_prefix(*id, writer)?;
+                inner.pretty_fmt(f, writer)?;
+                f.write_loop_postfix(writer)
+            }
+            AstKind::Seq(inners) => {
+                for inner in inners {
+                    inner.pretty_fmt(f, writer)?;
+                }
+                Ok(())
+            }
+            AstKind::LabeledBlock(id, inner) => {
+                f.write_labeled_block_prefix(*id, writer)?;
+                inner.pretty_fmt(f, writer)?;
+                f.write_labeled_block_postfix(writer)
+            }
+        }
+    }
+}
+
+struct PrettyFormatter<'a> {
     current_indent: usize,
     indent: &'a str,
-    loop_prefix: &'a str,
-    labeled_block_prefix: &'a str,
-    labeled_block_suffix: Option<&'a str>,
-    label_prefix: &'a str,
+    config: AstConfig<'a>,
 }
 
 impl<'a> PrettyFormatter<'a> {
-    pub fn new() -> Self {
-        PrettyFormatter::with_indent("  ")
-    }
-    pub fn with_indent(indent: &'a str) -> Self {
+    fn with_indent(indent: &'a str, config: AstConfig<'a>) -> Self {
         PrettyFormatter {
             current_indent: 0,
             indent: indent,
-            loop_prefix: "while (true) ",
-            labeled_block_prefix: "do ",
-            labeled_block_suffix: Some(" while(0)"),
-            label_prefix: "L",
+            config,
         }
     }
     fn write_node<W: io::Write>(
@@ -67,35 +149,7 @@ impl<'a> PrettyFormatter<'a> {
         self.current_indent += 1;
         writeln!(writer, "else {}", "{")
     }
-    fn write_loop_prefix<W: io::Write>(
-        &mut self,
-        id: usize,
-        writer: &mut W,
-    ) -> Result<(), io::Error> {
-        indent(writer, self.current_indent, self.indent)?;
-        write!(writer, "{}{}: {}", self.label_prefix, id, self.loop_prefix)?;
-        self.write_open_curly(writer)?;
-
-        self.current_indent += 1;
-        Ok(())
-    }
-    fn write_block_prefix<W: io::Write>(
-        &mut self,
-        id: usize,
-        writer: &mut W,
-    ) -> Result<(), io::Error> {
-        indent(writer, self.current_indent, self.indent)?;
-        write!(
-            writer,
-            "{}{}: {}",
-            self.label_prefix, id, self.labeled_block_prefix
-        )?;
-        self.write_open_curly(writer)?;
-
-        self.current_indent += 1;
-        Ok(())
-    }
-    fn write_block_suffix<W: io::Write>(
+    fn write_block_postfix<W: io::Write>(
         &mut self,
         writer: &mut W,
     ) -> Result<(), io::Error> {
@@ -106,27 +160,57 @@ impl<'a> PrettyFormatter<'a> {
 
         Ok(())
     }
-    fn write_labeled_block_suffix<W: io::Write>(
+    fn write_loop_prefix<W: io::Write>(
+        &mut self,
+        id: usize,
+        writer: &mut W,
+    ) -> Result<(), io::Error> {
+        indent(writer, self.current_indent, self.indent)?;
+        writeln!(
+            writer,
+            "{}{}: {}",
+            self.config.label_prefix, id, self.config.loop_prefix
+        )?;
+
+        self.current_indent += 1;
+        Ok(())
+    }
+    fn write_labeled_block_prefix<W: io::Write>(
+        &mut self,
+        id: usize,
+        writer: &mut W,
+    ) -> Result<(), io::Error> {
+        indent(writer, self.current_indent, self.indent)?;
+        writeln!(
+            writer,
+            "{}{}: {}",
+            self.config.label_prefix, id, self.config.labed_block_prefix
+        )?;
+
+        self.current_indent += 1;
+        Ok(())
+    }
+    fn write_loop_postfix<W: io::Write>(
         &mut self,
         writer: &mut W,
     ) -> Result<(), io::Error> {
         self.current_indent -= 1;
 
         indent(writer, self.current_indent, self.indent)?;
-        let suffix = if let Some(s) = self.labeled_block_suffix {
-            s
-        } else {
-            ""
-        };
-        writeln!(writer, "{}{}", "}", suffix)?;
+        writeln!(writer, "{}", self.config.loop_postfix)?;
 
         Ok(())
     }
-    fn write_open_curly<W: io::Write>(
+    fn write_labeled_block_postfix<W: io::Write>(
         &mut self,
         writer: &mut W,
     ) -> Result<(), io::Error> {
-        writeln!(writer, "{}", "{")
+        self.current_indent -= 1;
+
+        indent(writer, self.current_indent, self.indent)?;
+        writeln!(writer, "{}", self.config.labed_block_postfix)?;
+
+        Ok(())
     }
 }
 
